b31cff39eb0c36f6edf200fab2767b9cf36ae3eb
aaaaaa2493 <aaaaaa2493@yandex.ru>
2019-01-07 00:21:02 +0300

    Add possibility of drawing textures

--- src/server/blocks/Block.java
+++ src/server/blocks/Block.java
@@ -1,17 +1,32 @@
 package server.blocks;
 
 import java.awt.*;
+import java.util.Objects;
 
 public abstract class Block {
+
+    public final static Color noColor = new Color(0xCC60D7);
+
 
     public Block(){
 
     }
 
-    public abstract Color getColor();
+    public Color getColor() {
+        return noColor;
+    }
+
     public abstract String getId();
 
     public boolean isPassable() {
         return false;
+    }
+
+    public Image getTexture() {
+        return null;
+    }
+
+    public boolean hasTexture() {
+        return Objects.nonNull(getTexture());
     }
 }
